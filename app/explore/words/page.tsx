5cd98ac02d267d0facbe270b74f14ce12bb2ce55
ashutoshkr23 <ashutoshkumar.ash205@gmail.com>
2025-04-04 00:34:29 +0530

    fix

--- app/explore/words/page.tsx
+++ app/explore/words/page.tsx
@@ -19,6 +19,7 @@ export default function NotesPage() {
   useEffect(() => {
     setSelectedNote(null);
   }, [pathname]);
+  console.log(setNotes)
 
   const handleSwipe = (note: (typeof sampleNotes)[0]) => {
     setSelectedNote(note);
