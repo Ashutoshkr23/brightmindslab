201bb552219ce623334b2271e9948236a1e56fed
ashutoshkr23 <ashutoshkumar.ash205@gmail.com>
2025-06-04 11:39:43 +0530

    script added

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <meta name="monetag" content="ca6a1648193f22d8761c46647d004dfc"></meta>
+        <script src="https://fpyf8.com/88/tag.min.js" data-zone="150636" async data-cfasync="false"></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
